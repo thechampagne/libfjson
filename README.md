87d44874dfa6c86760ee0202cff49885f6040fa8
XXIV <13811862+thechampagne@users.noreply.github.com>
2023-01-16 02:46:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# libfjson
+
+[![](https://img.shields.io/github/v/tag/thechampagne/libfjson?label=version)](https://github.com/thechampagne/libfjson/releases/latest) [![](https://img.shields.io/github/license/thechampagne/libfjson)](https://github.com/thechampagne/libfjson/blob/main/LICENSE)
+
+A **C** library for parsing and formatting json with C-style comments and trailing commas.
+
+### Installation & Setup
+
+#### 1. Clone the repository
+```
+git clone https://github.com/thechampagne/libfjson.git
+```
+#### 2. Navigate to the root
+```
+cd libfjson
+```
+#### 3. Build the project
+```
+cargo build
+```
+
+### References
+ - [fjson](https://github.com/ryanfowler/fjson)
+
+### License
+
+This repo is released under the [MIT](https://github.com/thechampagne/libfjson/blob/main/LICENSE).
---
+++ src/lib.rs
@@ -0,0 +1,165 @@
+/*
+* Copyright (c) 2023 XXIV
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+use std::os::raw::c_char;
+use std::ffi::CStr;
+use std::ffi::CString;
+use fjson::to_json;
+use fjson::to_json_compact;
+use fjson::to_jsonc;
+
+#[repr(C)]
+#[allow(non_camel_case_types)]
+enum fjson_error_t {
+    FJSON_ERROR_RECURSION_LIMIT_EXCEEDED,
+    FJSON_ERROR_UNEXPECTED_CHARACTER,
+    FJSON_ERROR_UNEXPECTED_TOKEN,
+    FJSON_ERROR_UNEXPECTED_EOF,
+    FJSON_ERROR_WRITE,
+    FJSON_ERROR_NOT_VALID_UTF8,
+    FJSON_ERROR_NUL
+}
+
+#[no_mangle]
+unsafe extern "C" fn fjson_to_json(input: *const c_char, err_out: *mut fjson_error_t) -> *mut c_char {
+    let input_rs = match CStr::from_ptr(input).to_str() {
+	Ok(str) => str,
+	Err(_) => {
+	    (*err_out) = fjson_error_t::FJSON_ERROR_NOT_VALID_UTF8;
+	    return std::ptr::null_mut();
+	}
+    };
+    match to_json(input_rs) {
+	Ok(json_str) => match CString::new(json_str) {
+	    Ok(cstr) => cstr.into_raw(),
+	    Err(_) => {
+		(*err_out) = fjson_error_t::FJSON_ERROR_NUL;
+		std::ptr::null_mut()
+	    }
+	},
+	Err(err) => match err {
+	    fjson::error::Error::RecursionLimitExceeded => {
+		(*err_out) = fjson_error_t::FJSON_ERROR_RECURSION_LIMIT_EXCEEDED;
+		std::ptr::null_mut()
+	    },
+	    fjson::error::Error::UnexpectedCharacter(_, _) => {
+		(*err_out) = fjson_error_t::FJSON_ERROR_UNEXPECTED_CHARACTER;
+		std::ptr::null_mut()
+	    },
+	    fjson::error::Error::UnexpectedToken(_, _) => {
+		(*err_out) = fjson_error_t::FJSON_ERROR_UNEXPECTED_TOKEN;
+		std::ptr::null_mut()
+	    },
+	    fjson::error::Error::UnexpectedEOF => {
+		(*err_out) = fjson_error_t::FJSON_ERROR_UNEXPECTED_EOF;
+		std::ptr::null_mut()
+	    },
+	    fjson::error::Error::Write(_) => {
+		(*err_out) = fjson_error_t::FJSON_ERROR_WRITE;
+		std::ptr::null_mut()
+	    },
+	}
+    }
+}
+
+#[no_mangle]
+unsafe extern "C" fn fjson_to_json_compact(input: *const c_char, err_out: *mut fjson_error_t) -> *mut c_char {
+    let input_rs = match CStr::from_ptr(input).to_str() {
+	Ok(str) => str,
+	Err(_) => {
+	    (*err_out) = fjson_error_t::FJSON_ERROR_NOT_VALID_UTF8;
+	    return std::ptr::null_mut();
+	}
+    };
+    match to_json_compact(input_rs) {
+	Ok(json_str) => match CString::new(json_str) {
+	    Ok(cstr) => cstr.into_raw(),
+	    Err(_) => {
+		(*err_out) = fjson_error_t::FJSON_ERROR_NUL;
+		std::ptr::null_mut()
+	    }
+	},
+	Err(err) => match err {
+	    fjson::error::Error::RecursionLimitExceeded => {
+		(*err_out) = fjson_error_t::FJSON_ERROR_RECURSION_LIMIT_EXCEEDED;
+		std::ptr::null_mut()
+	    },
+	    fjson::error::Error::UnexpectedCharacter(_, _) => {
+		(*err_out) = fjson_error_t::FJSON_ERROR_UNEXPECTED_CHARACTER;
+		std::ptr::null_mut()
+	    },
+	    fjson::error::Error::UnexpectedToken(_, _) => {
+		(*err_out) = fjson_error_t::FJSON_ERROR_UNEXPECTED_TOKEN;
+		std::ptr::null_mut()
+	    },
+	    fjson::error::Error::UnexpectedEOF => {
+		(*err_out) = fjson_error_t::FJSON_ERROR_UNEXPECTED_EOF;
+		std::ptr::null_mut()
+	    },
+	    fjson::error::Error::Write(_) => {
+		(*err_out) = fjson_error_t::FJSON_ERROR_WRITE;
+		std::ptr::null_mut()
+	    },
+	}
+    }
+}
+
+#[no_mangle]
+unsafe extern "C" fn fjson_to_jsonc(input: *const c_char, err_out: *mut fjson_error_t) -> *mut c_char {
+    let input_rs = match CStr::from_ptr(input).to_str() {
+	Ok(str) => str,
+	Err(_) => {
+	    (*err_out) = fjson_error_t::FJSON_ERROR_NOT_VALID_UTF8;
+	    return std::ptr::null_mut();
+	}
+    };
+    match to_jsonc(input_rs) {
+	Ok(json_str) => match CString::new(json_str) {
+	    Ok(cstr) => cstr.into_raw(),
+	    Err(_) => {
+		(*err_out) = fjson_error_t::FJSON_ERROR_NUL;
+		std::ptr::null_mut()
+	    }
+	},
+	Err(err) => match err {
+	    fjson::error::Error::RecursionLimitExceeded => {
+		(*err_out) = fjson_error_t::FJSON_ERROR_RECURSION_LIMIT_EXCEEDED;
+		std::ptr::null_mut()
+	    },
+	    fjson::error::Error::UnexpectedCharacter(_, _) => {
+		(*err_out) = fjson_error_t::FJSON_ERROR_UNEXPECTED_CHARACTER;
+		std::ptr::null_mut()
+	    },
+	    fjson::error::Error::UnexpectedToken(_, _) => {
+		(*err_out) = fjson_error_t::FJSON_ERROR_UNEXPECTED_TOKEN;
+		std::ptr::null_mut()
+	    },
+	    fjson::error::Error::UnexpectedEOF => {
+		(*err_out) = fjson_error_t::FJSON_ERROR_UNEXPECTED_EOF;
+		std::ptr::null_mut()
+	    },
+	    fjson::error::Error::Write(_) => {
+		(*err_out) = fjson_error_t::FJSON_ERROR_WRITE;
+		std::ptr::null_mut()
+	    },
+	}
+    }
+}
